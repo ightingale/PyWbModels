381ec059c2233fc09ff9906e3c7dffeafbac8618
User <johnsena322@icloud.com>
2025-05-06 10:04:08 +0400

    feat: add advert info models

---
+++ src/pywbmodels/advert/campaigns/response/entity.py
@@ -0,0 +1,21 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class RawAdvertListItem:
+    advert_id: int
+    change_time: str
+
+
+@dataclass
+class RawAdvert:
+    type: int
+    status: int
+    count: int
+    advert_list: list[RawAdvertListItem]
+
+
+@dataclass
+class AdvertCampaignsResponse:
+    adverts: list[RawAdvert]
+    all: int
