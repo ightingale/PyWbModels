a121a530b076a579abeb95e9d4ad3152c9d61b59
User <johnsena322@icloud.com>
2025-06-26 16:44:49 +0400

    add new model

--- src/pywbmodels/seller_analytics/search_texts/response/entity.py
+++ src/pywbmodels/seller_analytics/search_texts/response/entity.py
@@ -1,10 +1,12 @@
 import datetime
 from dataclasses import dataclass
 
+from adaptix import Omitted
+
 from pywbmodels.seller_analytics.search_texts.response.value_objects import SearchTextsItemsData
 
 
 @dataclass
 class SearchTextsResponse:
     data: SearchTextsItemsData
-    period: datetime.date
+    period: datetime.date = Omitted()
